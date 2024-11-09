84b758109a41c12fe929710b6b040a0f1adc6259
Solpio <artemos267@gmail.com>
2024-11-09 23:04:14 +0400

    error fixes

--- src/pages/buyGift/index.tsx
+++ src/pages/buyGift/index.tsx
@@ -1,9 +1,8 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../redux/helpers.ts";
 import Card from "../../shared/components/Card";
 import { useTelegram } from "../../telegramAPI/hooks/useTelegram.ts";
 import { CardSizeEnum } from "../../shared/components/Card/Card.interface.ts";
-import { buyGift } from "../../shared/gifts/data";
 
 const BuyGiftPage = () => {
 	const { id } = useParams();
@@ -12,10 +11,8 @@ const BuyGiftPage = () => {
 		gifts: state.gifts.data,
 	}));
 
-	const navigate = useNavigate();
-
 	const gift = gifts.find((gift) => gift.id === id);
-	const { colorScheme, mainButton, user, switchInlineQuery } = useTelegram();
+	const { colorScheme, mainButton, switchInlineQuery } = useTelegram();
 
 	mainButton.onClick(() => {
 		// if (id) {
