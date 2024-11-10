65923210cb9498f0030d7b83bcbbe4e00b4572f0
Solpio <artemos267@gmail.com>
2024-11-10 22:01:02 +0400

    fix

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,30 +1,23 @@
-import { useParams } from "react-router-dom";
-import { useTelegram } from "../../telegramAPI/hooks/useTelegram.ts";
-import Profile from "../../shared/components/Profile";
-import { useAppDispatch, useAppSelector } from "../../redux/helpers.ts";
-import { useEffect } from "react";
-import { getMyProfile } from "../../shared/user/redux/user.slice.ts";
-
 const PageProfile = () => {
-	const { id } = useParams();
-	const { user } = useTelegram();
-
-	const { userProfile } = useAppSelector((state) => ({
-		userProfile: state.user.data,
-	}));
-
-	const dispatch = useAppDispatch();
-
-	useEffect(() => {
-		dispatch(getMyProfile());
-	}, []);
-
-	const mockData = {
-		leaderboardPosition: 1,
-		name: "Mark",
-	};
-
-	const isMyProfile = user.id === id;
+	// const { id } = useParams();
+	// const { user } = useTelegram();
+	//
+	// const { userProfile } = useAppSelector((state) => ({
+	// 	userProfile: state.user.data,
+	// }));
+	//
+	// const dispatch = useAppDispatch();
+	//
+	// useEffect(() => {
+	// 	dispatch(getMyProfile());
+	// }, []);
+	//
+	// const mockData = {
+	// 	leaderboardPosition: 1,
+	// 	name: "Mark",
+	// };
+	//
+	// const isMyProfile = user.id === id;
 	return (
 		<div>
 			{/*<Profile*/}
